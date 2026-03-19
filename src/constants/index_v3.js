44445338b3d13bfc057ab56cde09085a0d3d96ff
David <david@example.com>
2026-03-19 16:05:27 +0100

    Textos en majúscules

--- src/constants/index_v3.js
+++ src/constants/index_v3.js
@@ -7,9 +7,9 @@ export const INITIAL_ARTICLES = [
 
 export const MATERIA_SECA = {
     TAPS: ['COLAMATS', 'FLOR', 'DE FUSTA'],
-    AMPOLLES: ['BLANQUES', 'VERDES', 'CADAC', 'DOLÇOS', 'OLI', 'Magnum'],
-    CAPSULES: ['PICAPOLL', 'ROSAT', 'GX NEGRA', 'PERAFITA', 'CADAC', 'MISTEL·LA', 'GX DOLÇA', 'CAVA', 'CAVA PICAPOLL', 'Anònimes'],
-    CAIXES: ['picapoll', 'gx blanca', 'rosat', 'gx negra', 'merlot', 'perafita', 'mf', 'cadac', 'cadac magnum', 'dolços'],
+    AMPOLLES: ['BLANQUES', 'VERDES', 'CADAC', 'DOLÇOS', 'OLI', 'MAGNUM'],
+    CAPSULES: ['PICAPOLL', 'ROSAT', 'GX NEGRA', 'PERAFITA', 'CADAC', 'MISTEL·LA', 'GX DOLÇA', 'CAVA', 'CAVA PICAPOLL', 'ANÒNIMES'],
+    CAIXES: ['PICAPOLL', 'GX BLANCA', 'ROSAT', 'GX NEGRA', 'MERLOT', 'PERAFITA', 'MF', 'CADAC', 'CADAC MAGNUM', 'DOLÇOS'],
     ETIQUETES: [
         'PICAPOLL', 'GX BLANCA', 'ROSAT', 'GX NEGRA', 'MERLOT',
         'PERAFITA NEGRA', 'MF', 'CADAC', 'VERMUT', 'MISTELLA',
